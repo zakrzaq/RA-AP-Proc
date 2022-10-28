6c39962ed3239ecea3cd5c36ba0cbbd599bd5b41
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2022-10-28 16:08:57 +0200

    verify all but pce

--- APP/4_PM_STATUS.py
+++ APP/4_PM_STATUS.py
@@ -10,7 +10,27 @@ today = date.today().strftime("%m-%d-%Y")
 active = pd.read_excel('W:\AP MM Service Request Log.xlsm', sheet_name = 'Active Materials', dtype=str)
 selected_active_view = active[['Date Added', 'target sorg', 'target plant', 'email prefix\n(from request form)', 'SAP MATNR\n(from request form)', 'Service Requested\n(from request form)', 'Location\n(from request form)', 'Catalog', 'Ser', 'MTART/GenItemCat', ' sorg1k dchain', ' sorg1k cs', 'sorg1k price', ' sorg4k dchain', ' sorg4k cs', 'PGC', 'target sorg price', 'target sorg dchain', 'target sorg DWERK', 'target sorg cs', 'target sorg pub', 'target plant status', 'target plant mrp type', 'DWERK Plant Status', 'DWERK Plant Code', "mif/soerf check", 'Sales Text', 'INDIA GST\nINHTS', 'INDIA GST\nmarc.stuec', 'INDIA GST taxm1', 'STATUS_CHINA_ENERGY_LBL', "Regulatory Cert\n(Z62 Class)", 'Regulatory Cert\n(Z62 Characteristic)', 'Z62 characteristic\n(assigned in SAP)', 'PCE Assessment\n(received)', 'Date of PCE review', 'MIF Submitted', 'SOERF Submitted', 'pricing request', 'PCE cert rev req\'d', 'status', 'sort order']]
 
-#MATNRs GTS NEEDED
+# VERIFY PRICED
+selected_active_view.loc[
+  (selected_active_view['status'].str.contains('needs price;')) & (~selected_active_view['target sorg price'].isna()),
+  'status'
+] = selected_active_view['status'].str.replace('needs price;', '')
+
+# VERIFY PCE
+
+# VERIFY GTS
+selected_active_view.loc[
+  (selected_active_view['status'].str.contains('GST data needed;')) & ((~selected_active_view['INDIA GST\nINHTS'].isna()) | (~selected_active_view['INDIA GST\nmarc.stuec'].isna())),
+  'status'
+] = selected_active_view['status'].str.replace('GST data needed;', '')
+
+# VERIFY LOCAL
+selected_active_view.loc[
+  (selected_active_view['status'].str.contains('Localization required;')) & (~selected_active_view['target plant mrp type'].isin(['ND'])),
+  'status'
+] = selected_active_view['status'].str.replace('Localization required;', '')
+
+# MATNRs GTS NEEDED
 need_gts = ((selected_active_view['target sorg'].isin(['5008'])) & (selected_active_view['INDIA GST\nINHTS'].isna()) & (selected_active_view['INDIA GST\nmarc.stuec'].isna()) & (selected_active_view['status'].str.contains('cancel|complete|hold', case=False) == False) & (selected_active_view['status'].str.contains('GST data needed;') == False))
 selected_active_view.loc[need_gts, 'status'] = selected_active_view['status'].astype(str) + "GST data needed;"
 
