e5f1316511ae1fcb6c5c85476ed2dfc757b624af
Jakub Zakrzewski <jz.zakrzewsky@gmail.com>
2023-01-04 22:06:09 +0100

    import all tables on process

--- api/init_db.py
+++ api/init_db.py
@@ -1,6 +1,8 @@
+from helpers.helpers import output_msg
+
+
 def init_db():
     import os
-    import sqlite3
     import pandas as pd
 
     from helpers.db import con_db, close_db
@@ -14,13 +16,35 @@ def init_db():
 
     cur = con.cursor()
 
-    mara_f = os.path.join(os.getcwd(), 'INPUTS', 'mara.XLSX')
-    mara = pd.read_excel(mara_f)
+    # mara_f = os.path.join(os.getcwd(), 'INPUTS', 'mara.XLSX')
+    # mara = pd.read_excel(mara_f)
 
-    mara.to_sql('mara', con, if_exists='replace', index=False)
+    # mara.to_sql('mara', con, if_exists='replace', index=False)
 
-    all = cur.execute("SELECT * FROM mara").fetchall()
-    for r in all:
-        print(r[0:5])
+    output = ''
+    mara = cur.execute("SELECT * FROM mara").fetchall()
+    for row in mara:
+        print(row[0:4])
+    marc = cur.execute("SELECT * FROM marc").fetchall()
+    for row in marc:
+        print(row[0:4])
+    mvke = cur.execute("SELECT * FROM mvke").fetchall()
+    for row in mvke:
+        print(row[0:4])
+    ausp = cur.execute("SELECT * FROM ausp").fetchall()
+    for row in ausp:
+        print(row[0:4])
+    mlan = cur.execute("SELECT * FROM mlan").fetchall()
+    for row in mlan:
+        print(row[0:4])
+    gts = cur.execute("SELECT * FROM gts").fetchall()
+    for row in gts:
+        print(row[0:4])
+    price = cur.execute("SELECT * FROM price").fetchall()
+    for row in price:
+        print(row[0:4])
+    sales_text = cur.execute("SELECT * FROM sales_text").fetchall()
+    for row in sales_text:
+        print(row[0:4])
 
     close_db(con)
