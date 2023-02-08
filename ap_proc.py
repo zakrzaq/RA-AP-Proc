69ca548ba118622e2ecc16db2215845b3fb77cba
Jakub Zakrzewski <jz.zakrzewsky@gmail.com>
2023-02-08 13:12:12 +0100

    cli ui width

--- ap_proc.py
+++ ap_proc.py
@@ -33,30 +33,30 @@ clear()
 
 def get_menu_choice():
     def print_menu():  # Your menu design here
-        print(24 * "-", "AP MM EXTENSION PROCESS", 24 * "-")
+        print(20 * "-", "AP MM EXTENSION PROCESS", 20 * "-")
         print("1)    Get current requests")
         print("2)    Generate MIF / SOERF requests")
         print("3)    Update Material Statuses pre MM")
         print("4)    Generate AM Price & PCE requests")
-        print(78 * "-")
+        print(70 * "-")
         print("5)    Reconcile PCE / Update ORG Source")
         print("6)    Update Material Statuses post MM Extension")
         print("7)    Generate PM CCC, Localization & GTS Requests")
-        print(78 * "-")
+        print(70 * "-")
         print("8)    DATA: Download SAP Data")
         print("9)    DATA: Bring SAP Data")
-        print(78 * "-")
+        print(70 * "-")
         print("10)   UTILITY: Clean working folder & archive request files")
         print("11)   UTILITY: Check for MIF/SOERF submitted")
-        print("12)   UTILITY: Check for daily report uploaded to Sharepoint Repository")
+        print("12)   UTILITY: Check for daily report uploaded to Sharepoint Rep")
         print("13)   UTILITY: Open SAP Instance")
         print("14)   UTILITY: Open AP LOG file")
-        print(78 * "-")
+        print(70 * "-")
         print("tbl)  UTILITY: SAP data from table")
-        print(78 * "-")
+        print(70 * "-")
         print("U)    Update program")
         print("X)    Close program")
-        print(78 * "=")
+        print(70 * "=")
 
     loop = True
 
@@ -66,40 +66,40 @@ def get_menu_choice():
         print("\n")
 
         if choice == "1":
-            requests()
+            requests(server)
             clear()
         elif choice == "2":
-            mif_soerf()
+            mif_soerf(server)
             clear()
         elif choice == "3":
-            am_status()
+            am_status(server)
             clear()
         elif choice == "4":
-            am_emails()
+            am_emails(server)
             clear()
         elif choice == "5":
-            reconcile_pce()
+            reconcile_pce(server)
             clear()
         elif choice == "6":
-            pm_status()
+            pm_status(server)
             clear()
         elif choice == "7":
-            pm_emails()
+            pm_emails(server)
             clear()
         elif choice == "8":
-            get_sap_data()
+            get_sap_data(server)
             clear()
         elif choice == "9":
-            proc_sap_data()
+            proc_sap_data(server)
             clear()
         elif choice == "10":
-            clean_desktop()
+            clean_desktop(server)
             clear()
         elif choice == "11":
-            mif_soerf_check()
+            mif_soerf_check(server)
             clear()
         elif choice == "12":
-            check_daily_report()
+            check_daily_report(server)
             clear()
         elif choice == "13":
             os.system(r"C:\RA-Apps\AP-Proc\sap\sap.ahk")
