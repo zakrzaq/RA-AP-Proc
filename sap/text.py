34e0bb048247a1e3448d11cff1b7231fb51bc23d
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-05-26 20:59:11 +0200

    text

---
+++ sap/text.py
@@ -0,0 +1,46 @@
+from ahk import AHK
+from ahk.directives import NoTrayIcon
+import time
+import os
+from sap.open import get_sap
+from helpers.helpers import use_dotenv
+
+ahk = AHK(directives=[NoTrayIcon])
+# ahk.set_detect_hidden_windows(True)
+# ahk.set_title_match_mode(("RegEx", "Slow"))
+
+
+def text(table="SALES_TEXT"):
+    use_dotenv()
+    out_file = os.path.join(os.environ["DIR_IN"], f"{table}.XLSX")
+
+    try:
+        sap = get_sap()
+        if sap:
+            if os.path.exists(out_file):
+                os.remove(out_file)
+            sap.activate()
+            ahk.send_input("/oZ_COM_SALETXT_LD_EXT {Enter}")
+            ahk.win_wait_active("Material Sales Text Load/Extract")
+            time.sleep(1)
+            # select variant
+            ahk.send("+{F5}")
+            ahk.win_wait_active("Find Variant")
+            time.sleep(1)
+            ahk.send("{F8}")
+            time.sleep(1)
+            # paste parts
+            # TODO: tab not choosing correct button
+            ahk.send("{Tab} {Tab} {Tab} {Tab} {Tab}")
+            ahk.send("{Enter}")
+            ahk.win_wait_active("Multiple Selection for Material Number")
+            time.sleep(1)
+            ahk.send("+{F12}")
+            time.sleep(3)
+            ahk.send("{F8}")
+            time.sleep(3)
+            # execute
+            ahk.send("{F8}")
+
+    except TimeoutError:
+        print("failed to launch SAP!")
--- test.py
+++ test.py
@@ -2,10 +2,11 @@ from sap.se16 import se16
 from sap.gts import gts
 from sap.ih09 import ih09
 from sap.sqvi import sqvi
+from sap.text import text
 
 # tables = ["MARC", "MVKE", "AUSP", "MLAN"]
 
 # for t in tables:
 #     se16(t)
 
-sqvi()
+text()
