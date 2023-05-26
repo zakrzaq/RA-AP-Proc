cf3f002f510f9a1fa1990a00c92e9184358fc494
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-05-26 15:21:10 +0200

    sqvi

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from sap.se16 import se16
 from sap.gts import gts
 from sap.ih09 import ih09
+from sap.sqvi import sqvi
 
 # tables = ["MARC", "MVKE", "AUSP", "MLAN"]
 
 # for t in tables:
 #     se16(t)
 
-ih09()
+sqvi()
