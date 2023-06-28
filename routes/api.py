f50c23568b9f90ebb05ba7d03ba345905ceb3234
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-06-28 22:10:19 +0200

    api route & action_log

--- routes/api.py
+++ routes/api.py
@@ -1,6 +1,8 @@
+import os
 from flask import Blueprint, Response
 from flask_cors import cross_origin
 
+from utils.helpers import check_file
 from api.queries import get_json_data
 
 api_routes = Blueprint("api", __name__)
@@ -11,3 +13,15 @@ api_routes = Blueprint("api", __name__)
 def api_all():
     response = Response(get_json_data("select_all"))
     return response
+
+
+@api_routes.route("/api/action_log", methods=["GET"])
+@cross_origin()
+def api_action_log():
+    action_log_path = os.path.join(os.environ["DIR_LOG"], "action_log.txt")
+    check_file(action_log_path, create=True)
+    with open(action_log_path, "r") as file:
+        content = file.readlines()
+    for c in content:
+        c.replace("\n", "")
+    return content
